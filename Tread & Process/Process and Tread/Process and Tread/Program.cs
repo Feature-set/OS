e66e87b9015b1737a691d18deeb1730211b61b8a
DESKTOP-0LA1F5C\SSH <kory199@Gmail.com>
2023-05-24 10:30:19 +0900

    OS
    스레드 동작 정보

---
+++ Tread & Process/Process and Tread/Process and Tread/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+// 프로세스 및 이벤트 로깅 성능 모니터링 등이 포함된 네임스페이스
+using System.Diagnostics;
+
+namespace Process_and_Tread
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ProcessInfo();
+        }
+
+        static void ProcessInfo()
+        {
+            Process curProcess = Process.GetCurrentProcess();
+            Console.WriteLine("현재 프로세스 이름 : " + curProcess.ProcessName);
+            Console.WriteLine("현재 프로세스 ID (PID) : " + curProcess.Id);
+            Console.WriteLine("프로세스 시작 시간 : " + curProcess.StartTime);
+            Console.WriteLine("프로세스 실행 시간 : " + (DateTime.Now - curProcess.StartTime));
+        }
+    }
+}
